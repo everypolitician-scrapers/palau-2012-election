feeb5b5409d59d4e3b8fcfadbbf642d4f4d76b20
Tony Bowden <tony@tmtm.com>
2017-01-03 16:45:52 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -10,7 +11,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -22,20 +23,20 @@ def scrape_list(url)
   noko = noko_for(url)
   noko.xpath('//p[strong[text()="FOR HOUSE OF DELEGATES"]]/following-sibling::table').each do |table|
     area = table.css('tr').first.text.tidy
-    candidates = table.css('tr').drop(2).reject { |tr| tr.css('td').first.text.tidy.empty? }.map { |tr|
+    candidates = table.css('tr').drop(2).reject { |tr| tr.css('td').first.text.tidy.empty? }.map do |tr|
       tds = tr.css('td')
       {
-        name: tds[0].text.tidy,
-        area: area,
+        name:  tds[0].text.tidy,
+        area:  area,
         votes: tds[3].text.tidy,
-        party: "Independent",
-        won: 'no',
-        term: 2012,
+        party: 'Independent',
+        won:   'no',
+        term:  2012,
       }
-    }.sort_by { |c| c[:votes].to_i }.reverse
+    end.sort_by { |c| c[:votes].to_i }.reverse
     candidates.first[:won] = 'yes'
     # puts candidates
-    ScraperWiki.save_sqlite([:name, :area], candidates)
+    ScraperWiki.save_sqlite(%i(name area), candidates)
   end
 end
 
